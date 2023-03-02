95bc385e69bffcc58a74679580ab531966596a49
VishalChoudhary <vishalghn953@gmail.com>
2023-03-02 12:53:47 +0530

    file structure & dom-routing commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react'
+import Home from './components/Home';
+import MoviePage from './components/MoviePage';
+import Error from './components/Error';
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='movie/:id' element={<MoviePage />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MoviePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const MoviePage = () => {
+    const { id } = useParams();
+    return (
+        <>
+            <div>
+                Movie Page Working {id}
+            </div>
+        </>
+    );
+}
+
+export default MoviePage
